9b2343742db4d8a676174a2006f8d339ced73735
laharke <ronconimauro@yahoo.com.ar>
2021-07-20 21:24:09 -0300

    Changed the console based to use an UI with some imgs and buttons instead, logic works

--- functions.js
+++ functions.js
@@ -1,3 +1,42 @@
+let humanScore = 0;
+let computerScore = 0;
+const playerScoreNode = document.querySelector('#playerScore');
+const computerScoreNode = document.querySelector('#computerScore');
+const piedrabutton = document.querySelector('#piedra')
+
+const mensajeNode = document.querySelector('#mensaje')
+const winnerNode = document.querySelector('#winner')
+//meto los botones en una array
+const buttons = document.querySelectorAll('.btn');
+
+
+
+//loopeo por cada uno de los botones agragandole un listener para el click y que ahi se juege el juego sin encesidad de una loop
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+
+    let playerChoice = button.id;
+    let computerChoice = computerPlay();
+    let mensaje = playRound(playerChoice, computerChoice);
+    //updatear los scores cuando vayan cambiando
+    playerScoreNode.textContent = `Puntaje Jugador: ${humanScore}`;
+    computerScoreNode.textContent = `Puntaje Computadora: ${computerScore}`;
+    //Dar la informacion de que eligio cada uno y quien gano
+    mensajeNode.textContent = mensaje;
+    if (humanScore == 5 || computerScore == 5) {
+        //terminar el juego
+        buttons.forEach(button => button.disabled = true)
+        if (humanScore == 5){
+            winnerNode.textContent = 'GANASTE! F5 para jugar de nuevo'
+        }
+        if(computerScore == 5){}
+            winnerNode.textContent = 'PERDISTE! F5 para jugar de nuevo'
+    }
+    })
+})
+
+
+
+
 function computerPlay() {
     //funcion para simular una eleccion de la computadora, deuvlvo todo en minuscula 
     let number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
@@ -12,19 +51,13 @@ function computerPlay() {
     }
 }
 
-function playerSelection() {
-    //funcion para obtener input del jugador, probablmetne sea borrada por unos botones en el html y de ahi consigo la jugada, devuelve todo en minsucula
-    playerSelection = prompt("Piedra, papel o tijera");
-    playerSelection = playerSelection.toLowerCase();
-    return playerSelection;
-}
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection,) {
     //funcion que simula una ronda de piedra, papel o tijera y devuelve al ganador.
     //si gana el usuario
     if ((playerSelection == 'piedra' && computerSelection == 'tijera') ||
     (playerSelection == 'tijera' && computerSelection == 'papel') ||
     (playerSelection == 'papel' && computerSelection == 'piedra')) {
+        humanScore = humanScore + 1;
         return `Ganaste! ${playerSelection} le gana a ${computerSelection}`
     }
     //si empatan
@@ -34,10 +67,7 @@ function playRound(playerSelection, computerSelection) {
     }
     //si gana la maquina
     else {
+        computerScore = computerScore + 1;
         return `Perdiste! ${playerSelection} pierde con ${computerSelection}`
     }
 }
-
-let player = playerSelection();
-let computer = computerPlay();
-console.log(playRound(player, computer))
